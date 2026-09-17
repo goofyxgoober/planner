7e50e4475f35d44572b8f8b5b25434e8cd77a2d5
Braindeeeaad <indrajith2048@gmail.com>
2026-09-17 11:59:37 -0500

    starting handleSubmit

--- src/graph/Nodes.tsx
+++ src/graph/Nodes.tsx
@@ -1,15 +1,57 @@
 import { Handle, Position } from '@xyflow/react';
 import type {NodeProps} from '@xyflow/react';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
-import type { AppNode, GoalNodeData, HabitNodeData, TaskNodeData } from '../types';
+import type { AppNode, GoalNodeData, HabitNodeData, TaskNodeData, NodeType } from '../types';
+
+
+
+
+/*
+
+  How do I handle this????
+  Need to make a general FormElements that can take an unknown type of 
+
+*/ 
+interface FormElements extends HTMLFormControlsCollection {
+  usernameInput: HTMLInputElement
+  nodeType: NodeType
+}
+interface TaskElements extends HTMLFormControlsCollection {
+  goofInput: HTMLInputElement
+  nodeType: NodeType
+}
+
+
+interface UsernameFormElement extends HTMLFormElement{
+  readonly elements: FormElements | TaskElements 
+}
+
+
 
 function GoalNode({ data }: NodeProps<Extract<AppNode, { type: 'GOAL' }>>) {
   const { title, targetDate, status } = data.item;
+  
+  function handleSubmit(event: React.SubmitEvent<UsernameFormElement>) {
+    event.preventDefault()
+    const form = event.currentTarget
+    const formElements = form.elements as typeof form.elements & {
+      usernameInput: {value: string}
+    }
+    onSubmitUsername(formElements.usernameInput.value)
+    
+  }
+  
   return (
     <div className="rounded-lg border-2 border-purple-500 bg-purple-50 px-4 py-2 shadow-md">
       <div className="font-bold text-purple-900">{title}</div>
-      <div className="text-xs text-purple-700">Due {targetDate}</div>
-      <div className="text-xs text-purple-700">{status}</div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input className="text-xs text-purple-700" name="title">{title}</input>
+        <label htmlFor="due">Due:</label>
+        <input className="text-xs text-purple-700" name="due">{targetDate}</input>
+        <label htmlFor="status">Status:</label>
+        <input className="text-xs text-purple-700" name="status">{status}</input>
+      </form>
       <Handle type="source" position={Position.Bottom} />
     </div>
   );
